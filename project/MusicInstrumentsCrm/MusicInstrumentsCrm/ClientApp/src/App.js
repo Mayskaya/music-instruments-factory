d86f35f869657f78f4909099195f20eef08d6f13
Artyom Motozov <mav199824@gmail.com>
2019-11-03 14:05:29 +0300

    [-] Add TypeScript config, add Good model class

--- project/MusicInstrumentsCrm/MusicInstrumentsCrm/ClientApp/src/App.js
+++ project/MusicInstrumentsCrm/MusicInstrumentsCrm/ClientApp/src/App.js
@@ -11,7 +11,7 @@ export default class App extends Component {
   render() {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
+        <Route path='/' exact  component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetchdata' component={FetchData} />
       </Layout>
