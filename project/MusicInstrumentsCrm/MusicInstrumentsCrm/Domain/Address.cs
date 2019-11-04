df5eebe0f2071991e6dc4c0a0561183cad206f85
Artyom Motozov <mav199824@gmail.com>
2019-11-04 21:51:55 +0300

    [-] Fixed compilation errors

--- project/MusicInstrumentsCrm/MusicInstrumentsCrm/Domain/Address.cs
+++ project/MusicInstrumentsCrm/MusicInstrumentsCrm/Domain/Address.cs
@@ -1,6 +1,6 @@
 ﻿namespace MusicInstrumentsCrm.Domain
 {
-	public class Address
+	public class Address{
 	
 		public int Id { get; set; }
 
--- project/MusicInstrumentsCrm/MusicInstrumentsCrm/Startup.cs
+++ project/MusicInstrumentsCrm/MusicInstrumentsCrm/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using MusicInstrumentsCrm.Model;
+using MusicInstrumentsCrm.Domain;
 
 namespace MusicInstrumentsCrm
 {
