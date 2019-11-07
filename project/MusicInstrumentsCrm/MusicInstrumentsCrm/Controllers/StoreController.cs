cc57a9b2f74107e06d678709fe81990ff95e6867
Artyom Motozov <mav199824@gmail.com>
2019-11-07 18:02:18 +0300

    [-] Fixed missing semicolon

--- project/MusicInstrumentsCrm/MusicInstrumentsCrm/Controllers/StoreController.cs
+++ project/MusicInstrumentsCrm/MusicInstrumentsCrm/Controllers/StoreController.cs
@@ -11,7 +11,7 @@ namespace MusicInstrumentsCrm.Controllers
 	[Route("api/v1/[controller]")]
 	public class StoreController : Controller
 	{
-		private IStoreRepository storeRepository
+		private IStoreRepository storeRepository;
 		public StoreController(IStoreRepository storeRepository)
 		{
 			this.storeRepository = storeRepository;
