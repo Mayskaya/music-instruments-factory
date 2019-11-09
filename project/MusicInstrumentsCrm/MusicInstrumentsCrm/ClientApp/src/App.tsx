c68ede6e874e0601604c6989d39cbba019b0eae5
Artyom Motozov <mav199824@gmail.com>
2019-11-09 21:10:31 +0300

    [-] Fixed ClientApp

--- project/MusicInstrumentsCrm/MusicInstrumentsCrm/ClientApp/src/App.tsx
+++ project/MusicInstrumentsCrm/MusicInstrumentsCrm/ClientApp/src/App.tsx
@@ -1,27 +1,22 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { Route, Switch } from 'react-router';
 
 export default class App extends React.Component<{}, {}> {
-  private displayName: string = App.name;
-
   public render() {
-    return <Switch>
-      <Route path='/user' />
-      {/* <Route path='/' />
-      <Route path='/' />
-      <Route path='/' />
-      <Route path='/' />
-      <Route path='/' />
-      <Route path='/' />
-      <Route path='/' />
-      <Route path='/' />
-      <Route path='/' />
-      <Route path='/' />
-      <Route path='/' />
-      <Route path='/' />
-      <Route path='/' />
-      <Route path='/' /> */}
-    </Switch>;
+    return (<div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">Learn React</a>
+      </header>
+    </div>);
+
   }
 }
--- project/MusicInstrumentsCrm/MusicInstrumentsCrm/ClientApp/src/index.tsx
+++ project/MusicInstrumentsCrm/MusicInstrumentsCrm/ClientApp/src/index.tsx
@@ -1,19 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
-import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const rootElement = document.getElementById('root');
-
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    rootElement);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
