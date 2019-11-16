d3a755670568b979be228f8861965fe3b44180b7
Artyom Motozov <mav199824@gmail.com>
2019-11-16 23:00:25 +0300

    [-] Fixed schema.sql file, add 1_create_database_user.sql file

--- project/MusicInstrumentsCrm/MusicInstrumentsDb/src/main/sql/schema.sql
+++ project/MusicInstrumentsCrm/MusicInstrumentsDb/src/main/sql/schema.sql
@@ -1,3 +1,12 @@
+﻿/*
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    SHOULD BE FIRST IN SCHEMA.SQL
+  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ */
+CREATE USER admin SUPERUSER PASSWORD 'admin';
+CREATE DATABASE micrm_db;
+GRANT ALL PRIVILEGES ON DATABASE micrm_db TO admin;
+
 /** Address table*/
 CREATE TABLE Address
 (
