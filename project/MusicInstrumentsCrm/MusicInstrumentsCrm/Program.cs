9c305d14e316401f22ee1dc950c192948685dbae
Artyom Motozov <mav199824@gmail.com>
2019-11-16 22:52:05 +0300

    [-] Cleanup Program.cs file

--- project/MusicInstrumentsCrm/MusicInstrumentsCrm/Program.cs
+++ project/MusicInstrumentsCrm/MusicInstrumentsCrm/Program.cs
@@ -1,13 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Serilog;
 
 namespace MusicInstrumentsCrm
 {
@@ -18,8 +11,9 @@ namespace MusicInstrumentsCrm
 			CreateWebHostBuilder(args).Build().Run();
 		}
 
-		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-			WebHost.CreateDefaultBuilder(args)
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+		{
+			return WebHost.CreateDefaultBuilder(args)
 				.UseStartup<Startup>()
 				.ConfigureLogging(logging =>
 				{
@@ -28,7 +22,7 @@ namespace MusicInstrumentsCrm
 					logging.AddFilter("Microsoft", LogLevel.Warning);
 					logging.AddFilter("System", LogLevel.Warning);
 					logging.AddFilter("MusicInstrumentsCrm.Program", LogLevel.Debug);
-				
 				});
+		}
 	}
 }
