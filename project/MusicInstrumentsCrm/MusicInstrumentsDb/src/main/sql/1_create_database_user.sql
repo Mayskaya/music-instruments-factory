55178bbb8f3e062408e3e951d6b35af3a95f9314
Artyom Motozov <mav199824@gmail.com>
2019-11-16 23:03:39 +0300

    [-] Fixed schema.sql file, remove 1_create_database_user.sql file, fixed docker-compose.yml

--- project/MusicInstrumentsCrm/MusicInstrumentsDb/src/main/sql/1_create_database_user.sql
+++
@@ -1,9 +0,0 @@
-﻿/*
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    SHOULD BE FIRST IN SCHEMA.SQL
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- */
-CREATE USER admin SUPERUSER PASSWORD 'admin';
-CREATE DATABASE micrm_db;
-GRANT ALL PRIVILEGES ON DATABASE micrm_db TO admin;
-
--- project/MusicInstrumentsCrm/MusicInstrumentsDb/src/main/sql/schema.sql
+++ project/MusicInstrumentsCrm/MusicInstrumentsDb/src/main/sql/schema.sql
@@ -1,13 +1,4 @@
-﻿/*
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    SHOULD BE FIRST IN SCHEMA.SQL
-  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
- */
-CREATE USER admin SUPERUSER PASSWORD 'admin';
-CREATE DATABASE micrm_db;
-GRANT ALL PRIVILEGES ON DATABASE micrm_db TO admin;
-
-/** Address table*/
+﻿/** Address table*/
 CREATE TABLE Address
 (
     id           INTEGER      NOT NULL,
